483753031c203dcfef628b653eacf26057bf7a45
weavermonkey <vembarvarun@gmail.com>
2017-11-20 10:50:18 +0530

    Add html in python code - display variable in browser

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -4,4 +4,14 @@ from app import app
 @app.route('/index')
 
 def index():
-	return "Hello, World!"
+	user = {'nickname' : 'weavermonkey'}
+	return '''
+<html>
+	<head>
+		<title>Home Page</title>
+	</head>
+	<body>
+		<h1>Hello, ''' + user['nickname'] + '''</h1>
+	</body>
+</html>
+'''
